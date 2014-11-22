a6387acef1d4705deadc14023713b07762d96d03
Ramon de la Fuente <ramon@future500.nl>
2014-11-22 17:18:06 +0100

    added BookWasCheckedOut event

---
+++ src/Event/BookWasCheckedOut.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SWP\Exchange\Event;
+
+use SWP\Exchange\Book\BookId;
+use SWP\Exchange\Person\PersonId;
+
+class BookWasCheckedOut
+{
+    /** @var PersonId  */
+    private $borrowerId;
+
+    /** @var BookId  */
+    private $bookId;
+
+    /**
+     * @param PersonId $borrowerId
+     * @param BookId $bookId
+     */
+    public function __construct(PersonId $borrowerId, BookId $bookId)
+    {
+        $this->borrowerId = $borrowerId;
+        $this->bookId = $bookId;
+    }
+
+    /**
+     * @return PersonId
+     */
+    public function getBorrowerId()
+    {
+        return $this->borrowerId;
+    }
+
+    /**
+     * @return BookId
+     */
+    public function getBookId()
+    {
+        return $this->bookId;
+    }
+}
